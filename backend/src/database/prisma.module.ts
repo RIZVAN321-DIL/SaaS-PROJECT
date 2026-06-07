7edf3395c0159b96185ab34d028ec10c68dd8ddd
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-07 22:58:43 +0300

    Update prisma.module.ts

--- backend/src/database/prisma.module.ts
+++ backend/src/database/prisma.module.ts
@@ -1,1 +1,9 @@
-// TODO
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
