2ca8640afb28f3748a69f0115a4c9610096ae18d
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-07 23:00:08 +0300

    Update prisma.service.ts

--- backend/src/database/prisma.service.ts
+++ backend/src/database/prisma.service.ts
@@ -1,1 +1,17 @@
-// TODO
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+
+  // подключение к БД при старте приложения
+  async onModuleInit() {
+    await this.$connect();
+    console.log('🗄 Prisma connected to database');
+  }
+
+  // корректное закрытие соединения
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
