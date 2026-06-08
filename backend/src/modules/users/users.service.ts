ddce7af5472fb5c2a606d647c5b5863da9e2aaba
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-08 13:43:37 +0300

    Create users.service.ts

---
+++ backend/src/modules/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../database/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findById(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async create(email: string, password: string) {
+    return this.prisma.user.create({
+      data: { email, password },
+    });
+  }
+}
