ca8d1d369a7540645833a216ee3938a7b3f233f8
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-09 11:48:16 +0300

    Create create-case-type.dto.ts

---
+++ backend/src/modules/case-types/dto/create-case-type.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsArray,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateCaseTypeDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  organizationId: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsArray()
+  tasksTemplate?: string[];
+
+  @IsOptional()
+  @IsArray()
+  documentsTemplate?: string[];
+
+  @IsOptional()
+  @IsArray()
+  rules?: any[];
+}
