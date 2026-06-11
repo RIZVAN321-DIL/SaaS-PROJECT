28cea1ddd14ae1cedea44b7093d95aed935e3011
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-12 00:05:37 +0300

    Create refresh-token.dto.ts

---
+++ backend/src/modules/auth/dto/refresh-token.dto.ts
@@ -0,0 +1,14 @@
+import {
+  IsString,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class RefreshTokenDto {
+  @IsString()
+  @IsNotEmpty()
+  refreshToken: string;
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+}
