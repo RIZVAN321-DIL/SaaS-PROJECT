a03c3e1574e1bab3a7b0770bda882fffaf2c070f
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-12 15:11:01 +0300

    Update users.module.ts

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+
 import { PrismaModule } from '../../database/prisma.module';
 
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
 @Module({
-  imports: [PrismaModule],
-  providers: [UsersService],
-  controllers: [UsersController],
-  exports: [UsersService],
+  imports: [
+    PrismaModule,
+  ],
+  providers: [
+    UsersService,
+  ],
+  controllers: [
+    UsersController,
+  ],
+  exports: [
+    UsersService,
+  ],
 })
 export class UsersModule {}
