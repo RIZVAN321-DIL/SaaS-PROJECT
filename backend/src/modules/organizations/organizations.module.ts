5f000f243f7b3efbc4f2f6c3b48de6ca28213420
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-12 15:13:37 +0300

    Update organizations.module.ts

--- backend/src/modules/organizations/organizations.module.ts
+++ backend/src/modules/organizations/organizations.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { OrganizationsService } from './organizations.service';
-import { OrganizationsController } from './organizations.controller';
+
 import { PrismaModule } from '../../database/prisma.module';
 
+import { OrganizationsService } from './organizations.service';
+import { OrganizationsController } from './organizations.controller';
+
 @Module({
-  imports: [PrismaModule],
-  providers: [OrganizationsService],
-  controllers: [OrganizationsController],
-  exports: [OrganizationsService],
+  imports: [
+    PrismaModule,
+  ],
+  providers: [
+    OrganizationsService,
+  ],
+  controllers: [
+    OrganizationsController,
+  ],
+  exports: [
+    OrganizationsService,
+  ],
 })
 export class OrganizationsModule {}
