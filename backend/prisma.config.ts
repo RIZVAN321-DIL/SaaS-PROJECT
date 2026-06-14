dfef636055d674aa963aeeed8a79397cc6d042fd
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-14 17:47:03 +0300

    Delete backend/prisma.config.ts

--- backend/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@prisma/client/runtime/library';
-
-export default defineConfig({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL!,
-    },
-  },
-});
