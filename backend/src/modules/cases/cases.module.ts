62fdaab1cbebeb96c07615ee25cc2ea504f84aa0
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-14 20:57:56 +0300

    Update cases.module.ts

--- backend/src/modules/cases/cases.module.ts
+++ backend/src/modules/cases/cases.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CasesService } from './cases.service';
 import { CasesController } from './cases.controller';
 import { PrismaModule } from '../../database/prisma.module';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuditModule],
   providers: [CasesService],
   controllers: [CasesController],
   exports: [CasesService],
