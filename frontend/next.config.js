458aa08b91862ba9b8849ce6406e16eb1b487ee6
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-15 05:07:32 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  basePath: '/SaaS-PROJECT',
-  assetPrefix: '/SaaS-PROJECT/',
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
