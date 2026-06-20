f72d329832be72d80b9aab1b03f261a44c547ae9
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-20 20:21:28 +0300

    Update documents.module.ts

--- backend/src/modules/documents/documents.module.ts
+++ backend/src/modules/documents/documents.module.ts
@@ -1,35 +1,32 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
-
 import { PrismaModule } from '../../database/prisma.module';
-
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
 import { DocumentEncryptionService } from './document-encryption.service';
+import { S3StorageService } from './s3-storage.service';
 
 @Module({
   imports: [
     PrismaModule,
-
     MulterModule.register({
       limits: {
         fileSize: 50 * 1024 * 1024,
       },
     }),
   ],
-
   controllers: [
     DocumentsController,
   ],
-
   providers: [
     DocumentsService,
     DocumentEncryptionService,
+    S3StorageService,
   ],
-
   exports: [
     DocumentsService,
     DocumentEncryptionService,
+    S3StorageService,
   ],
 })
 export class DocumentsModule {}
