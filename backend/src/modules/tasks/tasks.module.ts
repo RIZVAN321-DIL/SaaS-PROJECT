796f80f9a4cf273cb3d5317df674c45211a19769
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-06-21 23:14:41 +0300

    Update tasks.module.ts

--- backend/src/modules/tasks/tasks.module.ts
+++ backend/src/modules/tasks/tasks.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { PrismaModule } from '../../database/prisma.module';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuditModule],
   providers: [TasksService],
   controllers: [TasksController],
   exports: [TasksService],
