39c53941b2fbbb85c7fd876ce6fed2da9d0120a0
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-07-02 12:59:35 +0300

    Update tasks.module.ts

--- backend/src/modules/tasks/tasks.module.ts
+++ backend/src/modules/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { PrismaModule } from '../../database/prisma.module';
 import { AuditModule } from '../audit/audit.module';
+import { PermissionsModule } from '../permissions/permissions.module';
 
 @Module({
-  imports: [PrismaModule, AuditModule],
+  imports: [PrismaModule, AuditModule, PermissionsModule],
   providers: [TasksService],
   controllers: [TasksController],
   exports: [TasksService],
