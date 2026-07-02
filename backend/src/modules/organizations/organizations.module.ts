db23cbfa84101728a04356e502caa921e139e8d3
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-07-02 10:44:48 +0300

    Update organizations.module.ts

--- backend/src/modules/organizations/organizations.module.ts
+++ backend/src/modules/organizations/organizations.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from '../../database/prisma.module';
+import { PermissionsModule } from '../permissions/permissions.module';
 
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
@@ -8,6 +9,7 @@ import { OrganizationsController } from './organizations.controller';
 @Module({
   imports: [
     PrismaModule,
+    PermissionsModule,
   ],
   providers: [
     OrganizationsService,
