272265451efd11321a4a3724280afa0be479efa7
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-07-05 14:13:08 +0300

    Update case-types.module.ts

--- backend/src/modules/case-types/case-types.module.ts
+++ backend/src/modules/case-types/case-types.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CaseTypesService } from './case-types.service';
 import { CaseTypesController } from './case-types.controller';
 import { PrismaModule } from '../../database/prisma.module';
+import { CaseStageModule } from '../case-stage/case-stage.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CaseStageModule],
   providers: [CaseTypesService],
   controllers: [CaseTypesController],
   exports: [CaseTypesService],
