7cd21f7abd0a07faf6114f5842df0f26c89b98dd
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-07-05 14:15:39 +0300

    Update clients.module.ts

--- backend/src/modules/clients/clients.module.ts
+++ backend/src/modules/clients/clients.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { PrismaModule } from '../../database/prisma.module';
+import { CustomFieldsModule } from '../custom-fields/custom-fields.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CustomFieldsModule],
   providers: [ClientsService],
   controllers: [ClientsController],
   exports: [ClientsService],
