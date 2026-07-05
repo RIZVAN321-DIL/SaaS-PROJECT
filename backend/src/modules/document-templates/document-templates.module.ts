1d13d6de51a5e31f3def5710d48a8d027236c34d
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-07-05 22:05:29 +0300

    Update document-templates.module.ts

--- backend/src/modules/document-templates/document-templates.module.ts
+++ backend/src/modules/document-templates/document-templates.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from '../../database/prisma.module';
+import { DocumentsModule } from '../documents/documents.module';
 
 import { DocumentTemplatesService } from './document-templates.service';
 import { DocumentTemplatesController } from './document-templates.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, DocumentsModule],
   providers: [DocumentTemplatesService],
   controllers: [DocumentTemplatesController],
   exports: [DocumentTemplatesService],
