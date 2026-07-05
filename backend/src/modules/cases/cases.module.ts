58f4e9e51b55466208efbb253f5745f2aad0f26b
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-07-05 14:05:38 +0300

    Update cases.module.ts

--- backend/src/modules/cases/cases.module.ts
+++ backend/src/modules/cases/cases.module.ts
@@ -4,9 +4,17 @@ import { CasesController } from './cases.controller';
 import { PrismaModule } from '../../database/prisma.module';
 import { AuditModule } from '../audit/audit.module';
 import { PermissionsModule } from '../permissions/permissions.module';
+import { CaseStageModule } from '../case-stage/case-stage.module';
+import { CustomFieldsModule } from '../custom-fields/custom-fields.module';
 
 @Module({
-  imports: [PrismaModule, AuditModule, PermissionsModule],
+  imports: [
+    PrismaModule,
+    AuditModule,
+    PermissionsModule,
+    CaseStageModule,
+    CustomFieldsModule,
+  ],
   providers: [CasesService],
   controllers: [CasesController],
   exports: [CasesService],
